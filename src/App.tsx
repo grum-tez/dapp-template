62afa5c4d226999dce30e79057f9f0e170ce36d5
grum-tez <grum.tez@gmail.com>
2023-01-26 12:56:11 +1300

    setup route to a basic page component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,19 @@ import { SettingsProvider } from './contexts/Settings';
 import { TaquitoProvider } from './contexts/Taquito';
 import { BeaconProvider } from './contexts/Beacon';
 import { ContractProvider } from './contexts/Contract';
-
+import { createBrowserRouter } from 'react-router-dom';
+import { Basic } from './routes/BasicPage';
+import { RouterProvider } from 'react-router';
+const router = createBrowserRouter([
+  {
+    path: "/basic",
+    element: <Basic />,
+  },
+  // {
+  //   path: "*",
+  //   element: <ErrorPage />,
+  // },
+]);
 
 
 function App() {
@@ -16,6 +28,13 @@ function App() {
           <BeaconProvider>
             <ContractProvider>
               <p>"hi"</p>
+              <RouterProvider router = {router} />
+
+
+
+
+
+
               {/* <MainPanel /> */}
             </ContractProvider>
           </BeaconProvider>
---
+++ src/routes/BasicPage.tsx
@@ -0,0 +1,7 @@
+
+export const Basic = () => {
+  return (<>
+          Welcome to the basic basic page
+          </>
+  )
+}
