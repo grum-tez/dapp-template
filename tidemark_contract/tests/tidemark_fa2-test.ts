ff183136bc6112b5d6457b19006d7d75894fc173
grum-tez <grum.tez@gmail.com>
2023-04-25 16:16:58 +1200

    bug

--- tidemark_contract/tests/tidemark_fa2-test.ts
+++ tidemark_contract/tests/tidemark_fa2-test.ts
@@ -1,6 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import * as att from '@completium/archetype-ts-types'
-import { greetName } from 'numinousk'
+// Hi! The two lines below cause the bug
+// import { greetName } from 'numinousk'
+// greetName('frank')
+//End of bug
+
 import {
   ArchetypeType,
   Rational,
@@ -25,7 +29,6 @@ BigNumber.config({ ROUNDING_MODE: BigNumber.ROUND_UP })
 import assert from 'assert'
 
 /* Accounts ---------------------------------------------------------------- */
-greetName('frank')
 const creator_one = get_account('creator_one')
 // const owner_one = get_account('owner_one')
 // const owner_two = get_account('owner_two')
